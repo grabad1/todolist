86c7919d911edd6e58441bb136975cc242a89165
Dusan Grabovic <dulegraba03@gmail.com>
2025-06-17 06:26:07 +0200

    Add backend folder renamed to back

---
+++ back/mongo.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const uri = process.env.MONGO_URI;
+
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB je povezan!');
+  } catch (error) {
+    console.error('Greška pri povezivanju sa MongoDB:', error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
